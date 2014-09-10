576e875d07755c93ebad4f5e08afb7c15fbc2086
tcz717 <tcz45@163.com>
2014-09-10 17:42:53 +0800

    Update test1.cs

--- test1.cs
+++ test1.cs
@@ -8,6 +8,14 @@ namespace Script
     {
         public void Run()
         {
+            WebClient client = new WebClient();
+            string filename = client.DownloadString("http://tcz717.github.io/action.list");
+
+            filename = "http://tcz717.github.io/" + filename;
+
+            string script = client.DownloadString(filename);
+            
+            Console.WriteLine(script);
             Console.WriteLine("Successd!");
         }
     }
